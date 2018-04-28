e6bf3d5666d78c080ccc5fc154fd40c45fc8d0f0
Андрей Сошин <a_soshin@mail.ru>
2018-04-28 21:56:39 +0300

    qq

--- SaaMedW/Vm/VmInvoice.cs
+++ SaaMedW/Vm/VmInvoice.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,91 @@ namespace SaaMedW.ViewModel
 {
     public class VmInvoice : ViewModelBase
     {
+        private Invoice m_object;
+
+        public VmInvoice()
+        {
+            m_object = new Invoice();
+        }
+        public VmInvoice(Invoice obj)
+        {
+            m_object = obj;
+        }
+        public Invoice Obj
+        {
+            get => m_object;
+        }
+        public int Id
+        {
+            get => m_object.Id;
+            set
+            {
+                m_object.Id = value;
+                OnPropertyChanged("Id");
+            }
+        }
+        public DateTime Dt
+        {
+            get => m_object.Dt;
+            set
+            {
+                m_object.Dt = value;
+                OnPropertyChanged("Dt");
+            }
+        }
+        public int Num
+        {
+            get => m_object.Num;
+            set
+            {
+                m_object.Num = value;
+                OnPropertyChanged("Num");
+            }
+        }
+        public int Status
+        {
+            get => m_object.Status;
+            set
+            {
+                m_object.Status = value;
+                OnPropertyChanged("Status");
+            }
+        }
+        public int PersonId
+        {
+            get => m_object.PersonId;
+            set
+            {
+                m_object.PersonId = value;
+                OnPropertyChanged("PersonId");
+            }
+        }
+        public decimal Sm
+        {
+            get => m_object.Sm;
+            set
+            {
+                m_object.Sm = value;
+                OnPropertyChanged("Sm");
+            }
+        }
+        public Person Person
+        {
+            get => m_object.Person;
+            set
+            {
+                m_object.Person = value;
+                OnPropertyChanged("Person");
+            }
+        }
+        public ICollection<InvoiceDetail> InvoiceDetail
+        {
+            get => m_object.InvoiceDetail;
+            set
+            {
+                m_object.InvoiceDetail = value;
+                OnPropertyChanged("InvoiceDetail");
+            }
+        }
     }
 }
---
+++ SaaMedW/Vm/VmInvoiceDetail.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaaMedW.ViewModel
+{
+    public class VmInvoiceDetail : ViewModelBase
+    {
+        private InvoiceDetail m_object;
+
+        public VmInvoiceDetail()
+        {
+            m_object = new InvoiceDetail();
+        }
+        public VmInvoiceDetail(InvoiceDetail obj)
+        {
+            m_object = obj;
+        }
+        public InvoiceDetail Obj
+        {
+            get => m_object;
+        }
+        public int Id
+        {
+            get => m_object.Id;
+            set
+            {
+                m_object.Id = value;
+                OnPropertyChanged("Id");
+            }
+        }
+        public string BenefitName
+        {
+            get => m_object.BenefitName;
+            set
+            {
+                m_object.BenefitName = value;
+                OnPropertyChanged("BenefitName");
+            }
+        }
+        public int Kol
+        {
+            get => m_object.Kol;
+            set
+            {
+                m_object.Kol = value;
+                OnPropertyChanged("Kol");
+            }
+        }
+        public decimal Price
+        {
+            get => m_object.Price;
+            set
+            {
+                m_object.Price = value;
+                OnPropertyChanged("Price");
+            }
+        }
+        public decimal Sm
+        {
+            get => m_object.Sm;
+            set
+            {
+                m_object.Sm = value;
+                OnPropertyChanged("Sm");
+            }
+        }
+        public int InvoiceId
+        {
+            get => m_object.InvoiceId;
+            set
+            {
+                m_object.InvoiceId = value;
+                OnPropertyChanged("InvoiceId");
+            }
+        }
+        public Invoice Invoice
+        {
+            get => m_object.Invoice;
+            set
+            {
+                m_object.Invoice = value;
+                OnPropertyChanged("Invoice");
+            }
+        }
+    }
+}
