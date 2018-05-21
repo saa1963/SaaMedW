e8a3fcfbb2297f8a774a96c770e3393d9c7e40e5
Андрей Сошин <a_soshin@mail.ru>
2018-05-21 22:57:03 +0300

    123

---
+++ SaaMedW/Models/PersonPartial.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaaMedW
+{
+    public partial class Person
+    {
+        public string Fio
+        {
+            get => LastName + " " + FirstName + (MiddleName != null ? " " : "") + MiddleName ?? "";
+        }
+        public string FioBirthday
+        {
+            get => (Fio + " " + (BirthDate.HasValue ? BirthDate.Value.ToString("dd.MM.yyyy") : "")).TrimEnd();
+        }
+    }
+}
