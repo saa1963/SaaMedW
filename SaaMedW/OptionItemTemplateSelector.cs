9eb2d23af419421b89a2fa27a1d17d9b1fb2ee3f
Андрей Сошин <a_soshin@mail.ru>
2018-07-30 16:52:56 +0300

    55

---
+++ SaaMedW/OptionItemTemplateSelector.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace SaaMedW
+{
+    public class OptionItemTemplateSelector: DataTemplateSelector
+    {
+        public DataTemplate TextDataTemplate { get; set; }
+        public DataTemplate DateTimeDataTemplate { get; set; }
+        public DataTemplate DecimalDataTemplate { get; set; }
+        public DataTemplate IntDataTemplate { get; set; }
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            if (item is int)
+                return IntDataTemplate;
+            else if (item is decimal)
+                return DateTimeDataTemplate;
+            else if (item is string)
+                return TextDataTemplate;
+            else if (item is DateTime)
+                return DateTimeDataTemplate;
+            else
+                return null;
+        }
+    }
+}
