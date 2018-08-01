6976e9a585db5a787c86427511c192bbdd5199af
Андрей Сошин <a_soshin@mail.ru>
2018-08-01 16:48:35 +0300

    33

--- SaaMedW/ViewModel/OptionsViewModel.cs
+++ SaaMedW/ViewModel/OptionsViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,35 @@ namespace SaaMedW.ViewModel
 {
     public class OptionsViewModel: ViewModelBase
     {
+        private SaaMedEntities ctx = new SaaMedEntities();
+        private ObservableCollection<NameValue> m_CommonParameterList
+            = new ObservableCollection<NameValue>();
+        private ObservableCollection<NameValue> m_UserParameterList
+            = new ObservableCollection<NameValue>();
+        public ObservableCollection<NameValue> CommonParameterList
+        {
+            get => m_CommonParameterList;
+            set
+            {
+                m_CommonParameterList = value;
+                OnPropertyChanged("CommonParameterList");
+            }
+        }
+        public ObservableCollection<NameValue> UserParameterList
+        {
+            get => m_UserParameterList;
+            set
+            {
+                m_UserParameterList = value;
+                OnPropertyChanged("UserParameterList");
+            }
+        }
+        public OptionsViewModel()
+        {
+            if (Global.Source.rUser.Role == 0)
+            {
+                ctx.Options.Where(s => s.UserId == 0)
+            }
+        }
     }
 }
