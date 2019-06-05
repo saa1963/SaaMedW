c1f2e1ae08668fcfaa96a5ae34f495cb260c373c
Андрей Сошин <a_soshin@mail.ru>
2019-06-05 17:02:12 +0300

    222

--- SaaMedW/Reports/Dogovor.cs
+++ SaaMedW/Reports/Dogovor.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using Xceed.Words.NET;
+//using Xceed.Words.NET;
+using Microsoft.Office.Interop.Word;
 
 namespace SaaMedW
 {
@@ -24,24 +25,37 @@ namespace SaaMedW
                     System.Reflection.Assembly.GetExecutingAssembly().Location), "templates", "ДОГОВОР 2019 для ПО.docx");
             var tmpName = Global.Source.GetTempFilename(".docx");
             File.Copy(templateName, tmpName);
-            using (var fs = new FileStream(tmpName, FileMode.Open, FileAccess.ReadWrite))
-            {
-                using (var doc = DocX.Load(fs))
-                {
-                    doc.InsertAtBookmark(numdog.ToString(), "numdog");
-                    doc.Bookmarks["numdog"].Paragraph.Font(new Font("Times New Roman")).FontSize(9);
-                    doc.InsertAtBookmark(DateTime.Today.ToString("dd.MM.yyyy") + " г.", "datedog");
-                    doc.Bookmarks["datedog"].Paragraph.Font(new Font("Times New Roman")).FontSize(9);
-                    doc.InsertAtBookmark(person.Fio, "patient");
-                    doc.Bookmarks["patient"].Paragraph.Font(new Font("Times New Roman")).FontSize(9);
+            var app = new Application();
+            var doc = app.Documents.Open(tmpName);
+            var r = doc.Bookmarks["numdog"].Range;
+            r.Text = numdog.ToString();
+            r.Font.Bold = 1;
+            r.Font.Italic = 1;
+            r.Font.Size = 12;
+            r = doc.Bookmarks["datedog"].Range;
+            r.Text = dt.ToString("dd.MM.yyyy") + " г.";
+            r.Font.Bold = 1;
+            r.Font.Italic = 1;
+            r.Font.Size = 12;
+            r = doc.Bookmarks["patient"].Range;
+            r.Text = person.Fio;
+            r.Font.Bold = 1;
+            r.Font.Italic = 1;
+            r.Font.Size = 12;
+            r = doc.Bookmarks["patient_address"].Range;
+            r.Text = person.FullAddress;
+            r.Font.Bold = 1;
+            r.Font.Italic = 1;
+            r.Font.Size = 12;
+            r = doc.Bookmarks["patient_phone"].Range;
+            r.Text = person.Phone;
+            r.Font.Bold = 1;
+            r.Font.Italic = 1;
+            r.Font.Size = 12;
+            doc.Save();
+            doc.Close();
+            app.Quit();
 
-                    doc.InsertAtBookmark(person.FullAddress, "patient_address");
-                    doc.Bookmarks["patient_address"].Paragraph.Font(new Font("Times New Roman")).FontSize(9);
-                    doc.InsertAtBookmark(person.Phone, "patient_phone");
-                    doc.Bookmarks["patient_phone"].Paragraph.Font(new Font("Times New Roman")).FontSize(9);
-                    doc.Save();
-                }
-            }
             return tmpName;
         }
     }
